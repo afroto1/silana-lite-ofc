3641e7f894fe0745861a60e96d7445ffe2a6b61d
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-19 07:09:29 +0200

    Update ارفعني.js

--- plugins/ارفعني.js
+++ plugins/ارفعني.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `*انت ادمن زعيم المكان يا مطورى 🌚♥️*`
+  if (is.Admin) throw `*انت ادمن زعيم المكان يا مطورى 🌚♥️*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
 handler.command = ["ارفعني"]
