f31a10d16499a1cfeb7b58a8026d662b1800a819
afroto1 <afrotoaldgalfo2@gmail.com>
2026-03-19 07:00:30 +0200

    Update ارفعني.js

--- plugins/ارفعني.js
+++ plugins/ارفعني.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw `*انت ادمن زعيم المكان يا مطورى 🌚♥️*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
-handler.command = /^ارفع|ارفعنى|ار|ارفعني|autoadmin$/i
+handler.command = ["ارفعني"]
 handler.rowner = true
 handler.botAdmin = true
 export default handler
